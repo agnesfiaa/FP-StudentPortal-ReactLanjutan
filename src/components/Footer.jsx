f57504a9e74876be26f35e5b85d0660b71c720bc
Muhammad Arif Faizin <muhammadariffaizin@gmail.com>
2023-07-28 10:08:41 +0700

    Init project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import { Box, Text, Flex } from "@chakra-ui/react";
+const Footer = () => {
+  return (
+    <Box className="footer">
+      <Flex
+        align="center"
+        justify="center"
+        
+        h="50px"
+        bg="gray.200"
+        borderTop="1px solid"
+        borderColor="gray.300"
+      >
+        <Text className="studentName" mr={2}>
+          Agnesfia Anggraeni
+        </Text>
+        <Text className="studentId">FE4930900</Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
+
